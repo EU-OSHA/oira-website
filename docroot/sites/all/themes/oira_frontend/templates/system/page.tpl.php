ea0957e8311a982bc8784f7c7959a1a1fb58f7a3
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-05-13 14:16:33 +0300

    Header region as in wireframes

--- docroot/sites/all/themes/oira_frontend/templates/system/page.tpl.php
+++ docroot/sites/all/themes/oira_frontend/templates/system/page.tpl.php
@@ -73,51 +73,51 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="navbar navbar-default container-fluid"><?php // print $navbar_classes; ?>
-  <div class="container">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-      <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
+<header class="container-fluid"><?php // print $navbar_classes; ?>
+  <div class="top-header row">
+      <div class="header-logo col-sm-4">
+        <?php if ($logo): ?>
+          <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
       </div>
-    <?php endif; ?>
+      <div class="header-sitename col-sm-4">
+        <?php if (!empty($site_name)): ?>
+          <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+        <?php endif; ?>
+      </div>
+      <div class="header-blocks col-sm-4">
+        <?php print render($page['header_block']); ?>
+      </div>
   </div>
+    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+      <div id="navbar" class="navbar navbar-default row">
+        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+<!--            --><?php //if (!empty($secondary_nav)): ?>
+<!--              --><?php //print render($secondary_nav); ?>
+<!--            --><?php //endif; ?>
+<!--            --><?php //if (!empty($page['navigation'])): ?>
+<!--              --><?php //print render($page['navigation']); ?>
+<!--            --><?php //endif; ?>
+          </nav>
+        </div>
+        <?php endif; ?>
+      </div>
 </header>
 
 <div class="main-container container-fluid">
-  <div>
-    <?php print render($page['above_header']); ?>
-  </div>
-
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
