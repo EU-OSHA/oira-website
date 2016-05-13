e4d36724dfa506b662d7373f80eed75557e51b03
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-05-13 13:42:57 +0300

    Practical tool filter by sector and filter links

--- docroot/sites/all/modules/oira/hwc_practical_tool/js/checkbox-submit.js
+++ docroot/sites/all/modules/oira/hwc_practical_tool/js/checkbox-submit.js
@@ -2,20 +2,13 @@
     Drupal.behaviors.osha_publication = {
         attach: function(context, settings) {
             var $form = $('#hwc-practical-tool-menu-tools-form');
-            $form.find('input[name=sort]').click(function(){
+            $form.find('input[type=checkbox]').click(function(){
                 $form.submit();
             });
-            $form.find('input[type=checkbox]').click(function(){
-                var $container = $(this).closest('.form-checkboxes');
-                if ($(this).val() != 0) {
-                    $container.find('[value=0]').prop('checked', false);
-                }
-                else {
-                    $container.find('[type=checkbox]').not(this).prop('checked', false);
-                }
-                if ($container.find(':checked').length == 0) {
-                    $container.find('[value=0]').prop('checked', true);
-                }
+            $form.find('input[type=select]').change(function(){
+                $form.submit();
+            });
+            $form.find('input[type=text]').focusout(function(){
                 $form.submit();
             });
         }
