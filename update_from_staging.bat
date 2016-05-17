60a752b11c9186159c9179d02bf6ce2ceee4f5a0
Andrei Melis <andrei.melis@eaudeweb.ro>
2016-05-17 19:52:06 +0300

    Fixes windows build

--- update_from_staging.bat
+++ update_from_staging.bat
@@ -6,12 +6,13 @@ cd docroot/
 call drush sql-drop -y
 
 rem Sync from edw staging
-call drush downsync_sql @osha.staging.sync @osha.local -y -v
+call drush downsync_sql @staging @self -y
 
 rem Devify - development settings
 call drush devify --yes
 call drush devify_solr
-call drush devify_ldap
 rem Build the site
-rem call drush osha_build -y
-call drush updatedb -y
+call drush osha_build -y
+call drush devify_ldap
+rem Sync files
+call drush -y rsync @staging:%files @self:%files
