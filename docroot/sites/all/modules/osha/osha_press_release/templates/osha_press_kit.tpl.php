893e52995516e07eeb4704eea94cfe89c85bfe2b
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-05-24 14:18:03 +0300

    Removed osha_press_release

--- docroot/sites/all/modules/osha/osha_press_release/templates/osha_press_kit.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="osha-press-contact-block-intro-text">
-  <?php print $osha_press_kit_intro_text; ?>
-</div>
-<div class="osha-press-contact-block-link">
-  <?php print $osha_press_kit_link; ?>
-</div>
