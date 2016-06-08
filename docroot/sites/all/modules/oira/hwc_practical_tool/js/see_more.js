8c51a46e2dfba74c3ed62e948a15e7caf4f9fe95
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-06-08 17:07:02 +0300

    See more details tools in search page

--- docroot/sites/all/modules/oira/hwc_practical_tool/js/see_more.js
+++ docroot/sites/all/modules/oira/hwc_practical_tool/js/see_more.js
@@ -3,19 +3,26 @@
         attach: function(context, settings) {
             $('.tools-see-more').once('tools_see_more', function() {
                 var expander = Drupal.settings.tools_expander;
+                console.log(expander);
                 var key = $(this).attr('data-nid');
-                var $wrapper = $(this).closest(expander[key].parent_selector);
+                var expander_setting = {};
+                $.each(expander, function(idx, value) {
+                    if (value.delta == key) {
+                        expander_setting = value;
+                    }
+                });
+                var $wrapper = $(this).closest(expander_setting.parent_selector);
                 var $body = $wrapper.find('.field-name-body');
                 var $partners = $wrapper.find('.field-name-related-partners');
                 $body.expander({
-                    slicePoint:       expander[key].slice_point,
+                    slicePoint:       expander_setting.slice_point,
                     expandPrefix:     '...',
                     expandText:       '', // default is 'read more'
                     userCollapseText: ''
                 });
                 $(this).on('click', function(){
-                    var show_less = expander[key].show_less_text;
-                    var show_more = expander[key].show_more_text;
+                    var show_less = expander_setting.show_less_text;
+                    var show_more = expander_setting.show_more_text;
                     if ($(this).hasClass('show-less')) {
                         $body.find('.read-less a').trigger('click');
                         $(this).text(Drupal.t(show_more));
