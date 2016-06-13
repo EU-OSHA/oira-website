3669e701a17a97e3901dddf4db9dc01ee6208593
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-06-13 15:42:40 +0300

    Partner content filter in country page

---
+++ docroot/sites/all/modules/oira/oira_partner/js/sector_filter.js
@@ -0,0 +1,40 @@
+(function($){
+    Drupal.behaviors.tool_sector_filter = {
+        attach: function (context, settings) {
+            $('#quicktabs-view__country_partner_content__block').once('tool_sector_filter', function() {
+                var sectors = [];
+                var used = [];
+                $('.sector-filter-tab-content > .node-teaser').each(function(){
+                    var sector_tid = $(this).attr('data-sector-tid');
+                    var sector_label = $(this).attr('data-sector-label');
+                    if ($.inArray(sector_tid, used) < 0) {
+                        used.push(sector_tid);
+                        sectors.push({sector_tid: sector_tid, sector_label: sector_label});
+                    }
+                });
+                var $ul = $('<ul class="partner-content-sector-filter"></ul>');
+                $ul.append('<li class="filter-active">' + Drupal.t("Any") + '</li>');
+                $.each(sectors, function(idx, value) {
+                    $ul.append('<li data-filter="' + value.sector_tid + '">' + value.sector_label + '</li>');
+                });
+                var tab = $('.sector-filter-tab-content').closest('.quicktabs-tabpage');
+                var index = tab.index('#quicktabs-container-view__country_partner_content__block > div');
+                $('ul.quicktabs-tabs > li').eq(index).append($ul);
+                $('.partner-content-sector-filter > li').on('click', function(e){
+                    e.preventDefault();
+                    $(this).closest('ul').closest('li').find('a').trigger('click');
+                    $(this).siblings('li').removeClass('filter-active');
+                    $(this).addClass('filter-active');
+                    var tid = $(this).attr('data-filter');
+                    if (typeof tid != 'undefined') {
+                        tab.find('.node-practical-tool.node-teaser').hide();
+                        tab.find('.node-practical-tool.node-teaser[data-sector-tid="' + tid + '"]').show();
+                    }
+                    else {
+                        tab.find('.node-practical-tool.node-teaser').show();
+                    }
+                });
+            });
+        }
+    };
+})(jQuery);
