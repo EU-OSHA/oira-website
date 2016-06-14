df268add3247e6cb81715c75a1aa13b1f5bffd47
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-06-14 18:08:22 +0300

    multple fixes tools,promotional materials

--- docroot/sites/all/modules/oira/hwc_practical_tool/js/see_more.js
+++ docroot/sites/all/modules/oira/hwc_practical_tool/js/see_more.js
@@ -13,6 +13,7 @@
                 var $wrapper = $(this).closest(expander_setting.parent_selector);
                 var $body = $wrapper.find('.field-name-body');
                 var $partners = $wrapper.find('.field-name-related-partners');
+                var $link = $wrapper.find('.field-name-field-tool-link');
                 $body.expander({
                     slicePoint:       expander_setting.slice_point,
                     expandPrefix:     '...',
@@ -27,11 +28,13 @@
                         $body.find('.read-less a').trigger('click');
                         $(this).text(Drupal.t(show_more));
                         $partners.slideUp();
+                        $link.hide();
                     }
                     else {
                         $body.find('.read-more a').trigger('click');
                         $(this).text(Drupal.t(show_less));
                         $partners.slideDown();
+                        $link.show();
                     }
                     $(this).toggleClass('show-less');
                 });
