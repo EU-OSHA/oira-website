961314b5d90a228b682ea8dd7a29d4e6222e08e6
Endika Llonin <ellonin@bilbomatica.es>
2016-06-15 12:23:03 +0200

    css changes oira tools- 15-06-2016 12:22

--- docroot/sites/all/themes/oira_frontend/js/oira-frontend.js
+++ docroot/sites/all/themes/oira_frontend/js/oira-frontend.js
@@ -1,45 +1,8 @@
 jQuery(document).ready(function () {
 
-    //dropdown OIRA TOOLS search menu
-    var visibleTool1= true;
-    var visibleTool2= true;
-    var visibleTool3= true;
+    //desplegable menu oira 
+    if(jQuery("#edit-field-languagevalue").is(":visible")){
+        jQuery("label[for='edit-field-languagevalue']").addClass("area-shown");
+    };
     
-    jQuery("#edit-field-country, #edit-field-languagevalue, #edit-field-sector").hide();
-    
-    jQuery("label[for='edit-field-country']").click(function(){
-        jQuery("#edit-field-country").slideToggle();
-        if(visibleTool1){
-            visibleTool1= false;
-            jQuery(this).addClass("label-row");
-            
-        }else{
-            visibleTool1= true;
-            jQuery(this).removeClass("label-row");
-        }
-        
-    });
-    jQuery("label[for='edit-field-languagevalue']").click(function(){
-        jQuery("#edit-field-languagevalue").slideToggle();
-        if(visibleTool2){
-            visibleTool2= false;
-            jQuery(this).addClass("label-row");
-            
-        }else{
-            visibleTool2= true;
-            jQuery(this).removeClass("label-row");
-        }
-    });
-    jQuery("label[for='edit-field-sector']").click(function(){
-        jQuery("#edit-field-sector").slideToggle();
-        if(visibleTool3){
-            visibleTool3= false;
-            jQuery(this).addClass("label-row");
-            
-        }else{
-            visibleTool3= true;
-            jQuery(this).removeClass("label-row");
-        }
-    });
-    //end dropdown Search menu
 });
