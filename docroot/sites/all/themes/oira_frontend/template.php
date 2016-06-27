a135caf74c98fd1aaac8d03c05a47a28a43bf4be
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-06-27 12:24:16 +0300

    add contaienr to tabs quicktabs

--- docroot/sites/all/themes/oira_frontend/template.php
+++ docroot/sites/all/themes/oira_frontend/template.php
@@ -448,3 +448,25 @@ function oira_frontend_top_anchor(&$vars) {
   );
   $vars['top_anchor'] = l('<img alt="Anchor to top" src="'.file_create_url(path_to_theme().'/images/anchor-top.png').'" />', '', $options);
 }
+
+function oira_frontend_qt_quicktabs_tabset($vars) {
+  $variables = array(
+    'attributes' => array(
+      'class' => 'quicktabs-tabs quicktabs-style-' . $vars['tabset']['#options']['style'],
+    ),
+    'items' => array(),
+  );
+  foreach (element_children($vars['tabset']['tablinks']) as $key) {
+    $item = array();
+    if (is_array($vars['tabset']['tablinks'][$key])) {
+      $tab = $vars['tabset']['tablinks'][$key];
+      if ($key == $vars['tabset']['#options']['active']) {
+        $item['class'] = array('active');
+      }
+      $item['data'] = drupal_render($tab);
+      $variables['items'][] = $item;
+    }
+  }
+  $ul = theme('item_list', $variables);
+  return '<div class="quicktabs-tabs-container container">' . $ul . '</div>';
+}
