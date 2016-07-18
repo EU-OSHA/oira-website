11d1681327ef444e1a6d0b57f2a6af5993d61735
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-07-18 13:52:56 +0300

    Fixes for search;

--- docroot/sites/all/themes/oira_frontend/template.php
+++ docroot/sites/all/themes/oira_frontend/template.php
@@ -193,7 +193,89 @@ function oira_frontend_pager($variables) {
   $variables['tags'][1] = '‹';
   $variables['tags'][3] = '›';
   $variables['tags'][4] = '»';
-  return theme_pager($variables);
+
+  $tags = $variables['tags'];
+  $element = $variables['element'];
+  $parameters = $variables['parameters'];
+  $quantity = $variables['quantity'];
+  global $pager_page_array, $pager_total;
+
+  // Calculate various markers within this pager piece:
+  // Middle is used to "center" pages around the current page.
+  $pager_middle = ceil($quantity / 2);
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // first is the first page listed by this pager piece (re quantity)
+  $pager_first = $pager_current - $pager_middle + 1;
+  // last is the last page listed by this pager piece (re quantity)
+  $pager_last = $pager_current + $quantity - $pager_middle;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
+
+  // Prepare for generation loop.
+  $i = $pager_first;
+
+  $li_first = theme('pager_first', array('text' => (isset($tags[0]) ? $tags[0] : t('« first')), 'element' => $element, 'parameters' => $parameters));
+  $li_previous = theme('pager_previous', array('text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+  $li_next = theme('pager_next', array('text' => (isset($tags[3]) ? $tags[3] : t('next ›')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
+  $li_last = theme('pager_last', array('text' => (isset($tags[4]) ? $tags[4] : t('last »')), 'element' => $element, 'parameters' => $parameters));
+
+  if ($pager_total[$element] > 1) {
+    if ($li_first) {
+      $items[] = array(
+        'class' => array('pager-first'),
+        'data' => $li_first,
+      );
+    }
+    if ($li_previous) {
+      $items[] = array(
+        'class' => array('pager-previous'),
+        'data' => $li_previous,
+      );
+    }
+
+  // When there is more than one page, create the pager list.
+    $items[] = array(
+      'class' => array('pager-current'),
+      'data' => $pager_current,
+    );
+
+    $items[] = array(
+      'class' => array('pager-separator'),
+      'data' => '/',
+    );
+
+    if ($pager_max - $pager_current) {
+      $items[] = array(
+        'class' => array('pager-item'),
+        'data' => theme('pager_next', array('text' => $pager_max, 'element' => $element, 'interval' => ($pager_max - $pager_current), 'parameters' => $parameters)),
+      );
+    }
+    else {
+      $items[] = array(
+        'class' => array('pager-item'),
+        'data' => $pager_current,
+      );
+    }
+    // End generation.
+    if ($li_next) {
+      $items[] = array(
+        'class' => array('pager-next'),
+        'data' => $li_next,
+      );
+    }
+    if ($li_last) {
+      $items[] = array(
+        'class' => array('pager-last'),
+        'data' => $li_last,
+      );
+    }
+    return '<h4 class="element-invisible">' . t('Pages') . '</h4>' . theme('item_list', array(
+      'items' => $items,
+      'attributes' => array('class' => array('pager')),
+    ));
+  }
 }
 
 /**
