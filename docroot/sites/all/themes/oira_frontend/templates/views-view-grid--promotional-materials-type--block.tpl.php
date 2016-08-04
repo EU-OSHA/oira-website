052636be4c896f368ef7df36387cde78473e1339
Claudia Ifrim <ifrim.claudia@gmail.com>
2016-08-04 17:26:44 +0300

    NO-66 - custom template for promotional resources intro page

---
+++ docroot/sites/all/themes/oira_frontend/templates/views-view-grid--promotional-materials-type--block.tpl.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)) : ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<div class="<?php print $class; ?>"<?php print $attributes; ?>>
+  <div class="row">
+  <?php foreach ($rows as $row_number => $columns): ?>
+    <?php $x = (int) (12 / $options['columns']); ?>
+      <?php foreach ($columns as $column_number => $item): ?>
+        <div class="col-md-<?php print $x; ?> col-sm-6 col-xs-12 <?php if ($column_classes[$row_number][$column_number]) { print $column_classes[$row_number][$column_number];  } ?>">
+          <div class="views-item-columns-container">
+            <?php print $item; ?>
+          </div>
+        </div>
+      <?php endforeach; ?>
+  <?php endforeach; ?>
+  </div>
+</div>
