095b3cfd9d42199e37a6b3f25b3300d85700d8b8
dragos-dumi <dumitrescu.dragos1@gmail.com>
2016-11-10 12:17:11 +0200

    Fix partner list closing markup

--- docroot/sites/all/modules/oira/oira_partner/js/partner_list.js
+++ docroot/sites/all/modules/oira/oira_partner/js/partner_list.js
@@ -3,9 +3,9 @@
         attach: function (context, settings) {
             $('.partner-logo-wrapper').once('country_partner_list', function() {
                 $(this).click(function() {
-                    $('.country-partner-wrapper').hide();
+                    $('.country-partner-wrapper').hide().removeClass('expanded');
                     var nid = $(this).attr('data-partner-nid');
-                    var shown_wrapper = $('.country-partner-wrapper[data-partner-nid=' + nid + ']').show();
+                    var shown_wrapper = $('.country-partner-wrapper[data-partner-nid=' + nid + ']').show().addClass('expanded');
                     $('html, body').animate({
                         scrollTop: shown_wrapper.offset().top
                     }, 800);
