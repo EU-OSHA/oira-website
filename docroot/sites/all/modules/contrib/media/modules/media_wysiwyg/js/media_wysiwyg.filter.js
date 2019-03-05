821c4745458e8f249881c7040ca299e2c8820086
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2019-03-05 12:41:13 +0200

    Media patched

--- docroot/sites/all/modules/contrib/media/modules/media_wysiwyg/js/media_wysiwyg.filter.js
+++ docroot/sites/all/modules/contrib/media/modules/media_wysiwyg/js/media_wysiwyg.filter.js
@@ -173,9 +173,8 @@
      */
     replacePlaceholderWithToken: function(content) {
       Drupal.media.filter.ensure_tagmap();
-
       // Locate and process all the media placeholders in the WYSIWYG content.
-      var contentElements = $('<div/>').html(content);  // TODO: once baseline jQuery is 1.8+, switch to using $.parseHTML(content)
+      var contentElements = $('<div>' + content + '</div>');  // TODO: once baseline jQuery is 1.8+, switch to using $.parseHTML(content)
       var mediaElements = contentElements.find('.media-element');
       if (mediaElements) {
         $(mediaElements).each(function (i) {
@@ -196,6 +195,15 @@
             // depending on the browser. Parsing tends to convert <img/> to <img>, for instance.)
             Drupal.settings.tagmap[macro] = markup;
             $(this).replaceWith(macro);
+          }
+        });
+        content = '';
+        contentElements.each(function(){
+          if ($(this)[0].localName == 'script') {
+            content += $(this)[0].outerHTML;
+          }
+          else {
+            content += $(this)[0].innerHTML;
           }
         });
         content = $(contentElements).html();
