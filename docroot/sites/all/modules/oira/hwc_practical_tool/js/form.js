11758503a54498bb8b39c64960f722f98cf7088b
Vsevolod Scovitin <scovitin.vsevolod@gmail.com>
2020-11-23 10:58:04 +0200

    MDR-4008 OiRA Web intelligence - Click on the 'Access the tool' link

--- docroot/sites/all/modules/oira/hwc_practical_tool/js/form.js
+++ docroot/sites/all/modules/oira/hwc_practical_tool/js/form.js
@@ -1,26 +1,26 @@
-(function($){
+(function ($) {
     Drupal.behaviors.tool_form_submit = {
-        attach: function(context, settings) {
-            $('.oira-custom-faceted-search-form').once('tool_form_submit', function(){
+        attach: function (context, settings) {
+            $('.oira-custom-faceted-search-form').once('tool_form_submit', function () {
                 var $form = $('.oira-custom-faceted-search-form');
-                $form.find('input[type=checkbox]').click(function(){
+                $form.find('input[type=checkbox]').click(function () {
                     $form.submit();
                 });
-                $form.find('select').change(function(){
+                $form.find('select').change(function () {
                     $form.submit();
                 });
-                $form.find('input[type=text]').focusout(function(){
+                $form.find('input[type=text]').focusout(function () {
                     $form.submit();
                 });
             });
         }
     }
     Drupal.behaviors.tool_form_toggle_facets = {
-        attach: function(context, settings) {
-            $('.oira-custom-faceted-search-form').once('tool_form_toggle_facets', function(){
+        attach: function (context, settings) {
+            $('.oira-custom-faceted-search-form').once('tool_form_toggle_facets', function () {
                 $('.form-checkboxes.search-facet-field').hide();
                 $('.form-checkboxes.search-facet-field').has('input:checked').show();
-                $('.form-checkboxes.search-facet-field').siblings('label').on('click', function() {
+                $('.form-checkboxes.search-facet-field').siblings('label').on('click', function () {
                     var $checkboxes = $(this).parent().find('.form-checkboxes.search-facet-field');
                     if ($checkboxes.is(':visible')) {
                         $checkboxes.slideUp();
@@ -37,16 +37,16 @@
     }
 
     Drupal.behaviors.tool_form_toggle_filter = {
-        attach: function(context, settings) {
-            $('.oira-custom-faceted-search-form').once('tool_form_toggle_filter', function() {
+        attach: function (context, settings) {
+            $('.oira-custom-faceted-search-form').once('tool_form_toggle_filter', function () {
                 $toggle = $('.toggle-search-sidebar');
                 $left_col = $('#edit-left-column');
                 $right_col = $('.search-page-results-wrapper');
 
-                $toggle.on('click', function() {
+                $toggle.on('click', function () {
                     $left_col.toggle();
                 });
-                $right_col.on('click', function() {
+                $right_col.on('click', function () {
                     if ($toggle.is(':visible') && $left_col.is(':visible')) {
                         $left_col.hide();
                     }
@@ -56,3 +56,32 @@
     }
 
 })(jQuery);
+
+function getTrackPath(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    var path = l.pathname;
+    var hostnames = [
+        'oiraproject.eu',
+        'test.oiraproject.eu',
+    ];
+
+    if (hostnames.includes(l.hostname)) {
+        if (l.pathname.substring(0, 11) === "/oira-tools") {
+            path = "/oira-tools" + l.pathname.substring(14);
+        }
+        else {
+            path = l.pathname.substring(3);
+        }
+    }
+    return 'https://' + l.hostname + path;
+}
+
+jQuery(document).ready(function ($) {
+    if (typeof _paq != 'undefined') {
+        jQuery('.page-oira-tools .field-name-field-tool-link a').click(function (e) {
+            var path = getTrackPath(jQuery(this).attr('href'));
+            _paq.push(['trackEvent', 'Banner', 'Click', path]);
+        });
+    }
+});
